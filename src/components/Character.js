7aefc5a7b3a2ea5334001c29b001809104f8de43
Edward <epfrndz@gmail.com>
2022-10-03 00:14:47 -0700

    Added health, stamina and gold in character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Character.css';
 
 const Character = (props) => {
+    // Name
+    const [name, setName] = useState(props.name)
+
+    // Health
+    const [health, setHealth] = useState('150')
+
+    // Stamina
+    const [stamina, setStamina] = useState('200')
+
+    // Gold
+    const [gold, setGold] = useState('75')
+
     return (
         <div>
-            <h2>{props.name}'s Bio:</h2>
+            <h2>{name}'s Bio:</h2>
             <p>Race: {props.race}</p>
-            <p>Status: {props.status}</p>
+            <p>Status: <br />
+                Health at {health}<br />
+                Stamina at {stamina}<br />
+            </p>
+            <p>Gold: {gold}</p>
             <p className={props.comment ? 'visible' : 'hidden'}>Comment: {props.comment}</p>
         </div>
     )
